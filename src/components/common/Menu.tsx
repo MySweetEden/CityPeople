dab571f3bf5dd2ad2eb82e6a7c1d9afef108a06d
Nyamadamadamada <n.yamadamadamada@gmail.com>
2022-11-13 13:42:12 +0900

    色々

--- src/components/common/Menu.tsx
+++ src/components/common/Menu.tsx
@@ -14,22 +14,22 @@ const Menu = () => {
     setPath(pathname);
   }, [pathname]);
   return (
-    <div className="bg-white w-full fixed top-0 h-16 items-center z-10">
-      <div className="flex flex-start border-b border-gray-900">
+    <div className="bg-[#68B7F5] w-full fixed top-0 h-16 items-center z-20">
+      <div className="flex flex-start border-b-2 border-gray-900">
         <div className="flex h-full">
           <Link
             to="/"
-            className="py-2 inline-flex items-center justify-center text-center w-52 border-r border-gray-900"
+            className="py-2 inline-flex items-center justify-center text-center w-52 border-r border-gray-900 bg-[#F9ED52]"
           >
             <div className="flex justify-center px-2">
               <p className="font-bold text-xl">都会マウント</p>
             </div>
           </Link>
           <Link
-            to="/detail"
+            to="/detail/test"
             className={`${
-              path == "/detail" ? "selectBtn" : "button"
-            } py-2 inline-flex items-center justify-center text-center w-52 border-r border-gray-900`}
+              path.indexOf("detail") !== -1 ? "selectBtn" : "button"
+            } leading-[4rem] py-2 inline-flex items-center justify-center text-center w-52 border-r border-gray-900`}
           >
             <div className="flex flex-col justify-center px-2">
               <p className=" font-bold text-xl">Detail</p>
@@ -40,7 +40,7 @@ const Menu = () => {
             to="/compare"
             className={`${
               path == "/compare" ? "selectBtn" : "button"
-            }  py-2 inline-flex items-center justify-center text-center w-52 border-r border-gray-900`}
+            } leading-[4rem] py-2 inline-flex items-center justify-center text-center w-52 border-r border-gray-900`}
           >
             <div className="flex flex-col justify-center px-2">
               <p className="font-bold text-xl">Compare</p>
--- src/components/common/Topbar.tsx
+++ src/components/common/Topbar.tsx
@@ -5,21 +5,52 @@ import Chart from "./Chart";
 
 type Props = {
   selectCity: [];
+  handleReset: () => void;
 };
-const Topbar = ({ selectCity }: Props) => {
+const Topbar = ({ selectCity, handleReset }: Props) => {
   return (
-    <div className="border-r border-gray-900 mt-16 z-40 h-full bg-white w-[40vw] p-4">
-      <h5 className="text-3xl font-semibold text-gray-500 my-4">
+    <div className="border-r-2 border-gray-900 pt-16 z-10  flex flex-col justify-between bg-[#68B7F5] w-[40vw] p-4 ">
+      <h5 className="text-3xl font-semibold text-gray-900 my-10">
         マウントしたい都市を選択してください
+        <div className="mx-auto bg-white border-2 border-gray-900 rounded-xl h-[40vh] mt-5 p-4">
+          <div className="flex flex-col mx-auto">
+            {selectCity.map((city, index) => (
+              <Link
+                to={`/detail/${city.key}`}
+                className={`${
+                  index == 0 ? "text-[#ff5eaf]" : "text-[#68B7F5]"
+                } hover:text-[#3D3B61] text-4xl pb-10 text-center`}
+              >
+                {city.prefecture_name} : {city.city_name}
+              </Link>
+            ))}
+          </div>
+          {selectCity.length == 2 && (
+            <div className="text-center">
+              <Link
+                to={`/compare`}
+                state={{
+                  city1: selectCity[0],
+                  city2: selectCity[1],
+                }}
+                className="text-center p-4 font-bold border-2 border-gray-900 m-auto w-28"
+              >
+                対決
+              </Link>
+            </div>
+          )}
+        </div>
       </h5>
-      <div className="">
-        {selectCity.map((city) => (
-          <p className="text-4xl pb-2">
-            {city.prefecture_name} : {city.city_name}
-          </p>
-        ))}
-      </div>
-      <div className="py-4 overflow-y-auto"></div>
+      {selectCity.length > 0 && (
+        <div className="">
+          <button
+            className="border-2 border-gray-900 h-auto w-28 bg-white"
+            onClick={handleReset}
+          >
+            リセット
+          </button>
+        </div>
+      )}
     </div>
   );
 };
